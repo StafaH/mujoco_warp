e97dd236b4298d8f9a53599b441d34ae2f90ba6c
Mustafa Haiderbhai <mhaid@cs.toronto.edu>
2025-10-08 14:43:31 -0400

    Update testspeed

--- mujoco_warp/_src/benchmark.py
+++ mujoco_warp/_src/benchmark.py
@@ -29,6 +29,7 @@ from . import io
 from . import warp_util
 from .types import Data
 from .types import Model
+from .render_context import RenderContext
 from .util_misc import halton
 
 
@@ -78,6 +79,7 @@ def benchmark(
   event_trace: bool = False,
   measure_alloc: bool = False,
   measure_solver_niter: bool = False,
+  render_context: Optional[RenderContext] = None,
 ) -> Tuple[float, float, dict, list, list, list, int]:
   """Benchmark a function of Model and Data.
 
@@ -111,8 +113,14 @@ def benchmark(
   with warp_util.EventTracer(enabled=event_trace) as tracer:
     # capture the whole function as a CUDA graph
     jit_beg = time.perf_counter()
-    with wp.ScopedCapture() as capture:
-      fn(m, d)
+
+    if render_context is not None:
+      with wp.ScopedCapture() as capture:
+        fn(m, d, render_context)
+    else:
+      with wp.ScopedCapture() as capture:
+        fn(m, d)
+
     jit_end = time.perf_counter()
     jit_duration = jit_end - jit_beg
 
